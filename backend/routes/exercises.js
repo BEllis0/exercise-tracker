0b90e9e2ec2c93ec6415495c66b9254639f6e113
Brandon Ellis <brandonellis@Brandons-MacBook-Pro.local>
2019-11-11 22:58:03 -0700

    finished routes

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -25,4 +25,35 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json("Error: " + err));
 });
 
+//handles getting the exercises
+router.route('/:id').get((req, res) => {
+    Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
+//handles deleting of exercises
+router.route('/:id').delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Exercise deleted"))
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
+//handles new exercises being added to db
+router.route('/update/:id').post((req, res) => {
+    Exercise.findById(req.params.id)
+    .then(exercise => {
+        exercise.username = req.body.username;
+        exercise.description = req.body.description;
+        exercise.duration = Number(req.body.duration);
+        exercise.date = Date.parse(req.body.date);
+
+        exercise.save()
+            .then(() => req.json('Exercise updated'))
+            .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
+
 module.exports = router;
