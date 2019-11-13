812522cabb312bd844f4a051e7a0298429d8233c
Brandon Ellis <brandonellis@Brandons-MacBook-Pro.local>
2019-11-13 09:28:47 -0700

    Created component files and routed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/navbar.component.js";
+import CreateExercise from "./components/create-exercise.component.js";
+import CreateUser from "./components/create-user.component.js";
+import EditExercise from "./components/edit-exercises.component.js";
+import ExercisesList from "./components/exercises-list.component.js";
 
 function App() {
   return (
-    <div className="App">
-      asdf
-    </div>
+    <Router>
+      <Navbar />
+      <br />
+      <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
+    </Router>
   );
 }
 
