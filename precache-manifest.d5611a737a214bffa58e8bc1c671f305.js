815bcf3cf4898202c982004f035cf43c10e56989
Brandon Ellis <adk_skier@yahoo.com>
2019-12-07 18:08:10 -0700

    Updates

---
+++ precache-manifest.d5611a737a214bffa58e8bc1c671f305.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b6fdf6f3c626cca4a269ac74c6ea20",
+    "url": "/exercise-tracker/index.html"
+  },
+  {
+    "revision": "3daea68d6f4eb333472e",
+    "url": "/exercise-tracker/static/css/2.4e80ca6c.chunk.css"
+  },
+  {
+    "revision": "4f5160b4be162c174c9c",
+    "url": "/exercise-tracker/static/css/main.2680dfa7.chunk.css"
+  },
+  {
+    "revision": "3daea68d6f4eb333472e",
+    "url": "/exercise-tracker/static/js/2.2d10bf64.chunk.js"
+  },
+  {
+    "revision": "4f5160b4be162c174c9c",
+    "url": "/exercise-tracker/static/js/main.e7e0b2b4.chunk.js"
+  },
+  {
+    "revision": "9a50bcc671dde963d01d",
+    "url": "/exercise-tracker/static/js/runtime-main.300ad1eb.js"
+  }
+]);
